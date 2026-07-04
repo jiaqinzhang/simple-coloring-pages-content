8d7252e3d33a75658bef2ef50b87273a489ba1e8
Simple Coloring Pages <dev@simplecoloringpagesforkids.com>
2026-07-04 16:26:17 +0800

    Add preview batch WP-CLI importer

---
+++ import_preview_batch.php
@@ -0,0 +1,183 @@
+<?php
+// Auto-generated preview batch importer. Safe to re-run (skips themes that already exist).
+if ( ! defined('WP_CLI') ) { echo "Must run via wp-cli
+"; exit(1); }
+
+$themes = [];
+
+$themes[] = [
+  'slug' => 'dinosaur',
+  'title' => 'Dinosaur Coloring Pages',
+  'category' => 'Animals',
+  'tint' => '#DCEEFB',
+  'intro' => 'Roar into fun with our dinosaur coloring pages! From friendly baby dinos to more detailed prehistoric designs, there's a page for every young paleontologist.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dinosaur_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Dinosaur Coloring Page', 'alt' => 'Easy printable dinosaur coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dinosaur_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dinosaur_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Dinosaur Coloring Page', 'alt' => 'Medium printable dinosaur coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dinosaur_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dinosaur_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Dinosaur Coloring Page', 'alt' => 'Detailed printable dinosaur coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dinosaur_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dinosaur_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+$themes[] = [
+  'slug' => 'cat',
+  'title' => 'Cat Coloring Pages',
+  'category' => 'Animals',
+  'tint' => '#FFF3D6',
+  'intro' => 'Sweet, cuddly kittens ready to be colored in! A purr-fect coloring activity for cat-loving kids of all ages.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/cat_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Cat Coloring Page', 'alt' => 'Easy printable cat coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/cat_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/cat_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Cat Coloring Page', 'alt' => 'Medium printable cat coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/cat_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/cat_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Cat Coloring Page', 'alt' => 'Detailed printable cat coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/cat_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/cat_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+$themes[] = [
+  'slug' => 'dog',
+  'title' => 'Dog Coloring Pages',
+  'category' => 'Animals',
+  'tint' => '#DFF3E8',
+  'intro' => 'Adorable puppies waiting for their colors! These friendly dog coloring pages are great for any kid who loves animals.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dog_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Dog Coloring Page', 'alt' => 'Easy printable dog coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dog_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dog_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Dog Coloring Page', 'alt' => 'Medium printable dog coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dog_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dog_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Dog Coloring Page', 'alt' => 'Detailed printable dog coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dog_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/dog_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+$themes[] = [
+  'slug' => 'rabbit',
+  'title' => 'Rabbit Coloring Pages',
+  'category' => 'Animals',
+  'tint' => '#FDE4EE',
+  'intro' => 'Hop into coloring with our cute bunny pages! Simple, sweet rabbit designs perfect for little hands.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/rabbit_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Rabbit Coloring Page', 'alt' => 'Easy printable rabbit coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/rabbit_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/rabbit_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Rabbit Coloring Page', 'alt' => 'Medium printable rabbit coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/rabbit_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/rabbit_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Rabbit Coloring Page', 'alt' => 'Detailed printable rabbit coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/rabbit_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/rabbit_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+$themes[] = [
+  'slug' => 'unicorn',
+  'title' => 'Unicorn Coloring Pages',
+  'category' => 'Fantasy & Cute',
+  'tint' => '#EDE6FA',
+  'intro' => 'Magical unicorns ready for a rainbow of colors! A dreamy coloring activity for any unicorn fan.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/unicorn_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Unicorn Coloring Page', 'alt' => 'Easy printable unicorn coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/unicorn_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/unicorn_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Unicorn Coloring Page', 'alt' => 'Medium printable unicorn coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/unicorn_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/unicorn_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Unicorn Coloring Page', 'alt' => 'Detailed printable unicorn coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/unicorn_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/unicorn_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+$themes[] = [
+  'slug' => 'princess',
+  'title' => 'Princess Coloring Pages',
+  'category' => 'Fantasy & Cute',
+  'tint' => '#FFE9DC',
+  'intro' => 'Royal fun awaits with our princess coloring pages! Simple, sweet designs fit for any little royal.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/princess_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Princess Coloring Page', 'alt' => 'Easy printable princess coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/princess_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/princess_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Princess Coloring Page', 'alt' => 'Medium printable princess coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/princess_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/princess_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Princess Coloring Page', 'alt' => 'Detailed printable princess coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/princess_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/princess_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+$themes[] = [
+  'slug' => 'truck',
+  'title' => 'Truck Coloring Pages',
+  'category' => 'Vehicles',
+  'tint' => '#DCEEFB',
+  'intro' => 'Vroom into coloring with our friendly truck pages! Great for kids who love vehicles and things that go.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/truck_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Truck Coloring Page', 'alt' => 'Easy printable truck coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/truck_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/truck_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Truck Coloring Page', 'alt' => 'Medium printable truck coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/truck_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/truck_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Truck Coloring Page', 'alt' => 'Detailed printable truck coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/truck_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/truck_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+$themes[] = [
+  'slug' => 'christmas',
+  'title' => 'Christmas Coloring Pages',
+  'category' => 'Holidays',
+  'tint' => '#FFF3D6',
+  'intro' => 'Spread holiday cheer with our Christmas coloring pages! Festive, simple designs perfect for the season.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/christmas_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Christmas Coloring Page', 'alt' => 'Easy printable christmas coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/christmas_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/christmas_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Christmas Coloring Page', 'alt' => 'Medium printable christmas coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/christmas_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/christmas_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Christmas Coloring Page', 'alt' => 'Detailed printable christmas coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/christmas_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/christmas_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+$themes[] = [
+  'slug' => 'birthday',
+  'title' => 'Birthday Coloring Pages',
+  'category' => 'Holidays',
+  'tint' => '#DFF3E8',
+  'intro' => 'Celebrate with our birthday coloring pages! Cake, candles, and balloons make for a fun party activity.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/birthday_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Birthday Coloring Page', 'alt' => 'Easy printable birthday coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/birthday_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/birthday_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Birthday Coloring Page', 'alt' => 'Medium printable birthday coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/birthday_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/birthday_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Birthday Coloring Page', 'alt' => 'Detailed printable birthday coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/birthday_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/birthday_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+$themes[] = [
+  'slug' => 'alphabet',
+  'title' => 'Alphabet Coloring Pages',
+  'category' => 'Educational',
+  'tint' => '#FDE4EE',
+  'intro' => 'Learn letters while having fun! Our alphabet coloring pages combine early literacy with coloring, starting with the letter A.',
+  'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/alphabet_easy_letter.png',
+  'pages' => [
+    [ 'title' => 'Easy Alphabet Coloring Page', 'alt' => 'Easy printable alphabet coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/alphabet_easy_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/alphabet_easy_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Medium Alphabet Coloring Page', 'alt' => 'Medium printable alphabet coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/alphabet_medium_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/alphabet_medium_letter.png', 'pdf_url' => '' ],
+    [ 'title' => 'Detailed Alphabet Coloring Page', 'alt' => 'Detailed printable alphabet coloring page for kids', 'thumb_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/alphabet_detailed_letter.png', 'png_url' => 'https://raw.githubusercontent.com/jiaqinzhang/simple-coloring-pages-content/master/images/alphabet_detailed_letter.png', 'pdf_url' => '' ]
+  ],
+];
+
+foreach ( $themes as $t ) {
+    $existing = get_page_by_title( $t['title'], OBJECT, 'coloring_topic' );
+    if ( $existing ) {
+        echo "SKIP (exists): " . $t['title'] . "
+";
+        continue;
+    }
+    $post_id = wp_insert_post( [
+        'post_type'   => 'coloring_topic',
+        'post_title'  => $t['title'],
+        'post_status' => 'publish',
+    ] );
+    if ( is_wp_error( $post_id ) ) {
+        echo "ERROR creating " . $t['title'] . ": " . $post_id->get_error_message() . "
+";
+        continue;
+    }
+    $term = term_exists( $t['category'], 'topic_category' );
+    if ( ! $term ) {
+        $term = wp_insert_term( $t['category'], 'topic_category' );
+    }
+    if ( ! is_wp_error( $term ) ) {
+        wp_set_post_terms( $post_id, [ (int) $term['term_id'] ], 'topic_category' );
+    }
+    update_post_meta( $post_id, 'scp_thumb_url', $t['thumb_url'] );
+    update_post_meta( $post_id, 'scp_tint', $t['tint'] );
+    update_post_meta( $post_id, 'scp_intro', $t['intro'] );
+    update_post_meta( $post_id, 'scp_pdf_all_url', '' );
+    update_post_meta( $post_id, 'scp_pdf_all_size', '' );
+    update_post_meta( $post_id, 'scp_age_range', '2-10' );
+    update_post_meta( $post_id, 'scp_pages', $t['pages'] );
+    echo "CREATED: " . $t['title'] . " (ID $post_id, " . count($t['pages']) . " pages)
+";
+}
+echo "DONE
+";
